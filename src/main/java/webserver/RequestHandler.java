2aee471cb0504396a4cdb0495fcabeb56cc34d5d
Bombo <bombo96@chungwoon.ac.kr>
2023-12-11 16:54:31 +0900

    feat: feat : GET 요청으로 유저 회원가입 하기
    요구사항2 해결
    - 유저에 대한 QueryString을 입력받아 User 객체 생성

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,69 +10,90 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import util.HttpHeaderUtils;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
-	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-	private final Socket connection;
+    private static final String ROOT_DIRECTORY = "./webapp";
+    private final Socket connection;
 
-	public RequestHandler(Socket connectionSocket) {
-		this.connection = connectionSocket;
-	}
+    public RequestHandler(Socket connectionSocket) {
+        this.connection = connectionSocket;
+    }
 
-	@Override
-	public void run() {
-		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-			connection.getPort());
+    @Override
+    public void run() {
+        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+                connection.getPort());
 
-		try (
-			InputStream in = connection.getInputStream();
-			OutputStream out = connection.getOutputStream()
-		) {
-			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			DataOutputStream dos = new DataOutputStream(out);
+        try (
+                InputStream in = connection.getInputStream();
+                OutputStream out = connection.getOutputStream()
+        ) {
+            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            DataOutputStream dos = new DataOutputStream(out);
 
-			String line = br.readLine();
-			if (line == null) {
-				return;
-			}
-			String url = HttpHeaderUtils.parseUrl(line);
-			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-			while (!"".equals(line)) {
-				line = br.readLine();
-				System.out.println(line);
-			}
+            String line = br.readLine();
+            if (line == null) {
+                return;
+            }
+            String url = HttpHeaderUtils.parseUrl(line);
+            String queryString = "";
 
-			response200Header(dos, body.length);
-			responseBody(dos, body);
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
-	}
+            if (url.contains(".html")) {
+                byte[] body = Files.readAllBytes(new File(ROOT_DIRECTORY + url).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
 
-	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-		try {
-			dos.writeBytes("HTTP/1.1 200 OK \r\n");
-			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-			dos.writeBytes("\r\n");
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
-	}
+            if (url.contains("?")) {
+                int queryStringIndex = url.indexOf('?');
+                queryString = url.substring(queryStringIndex + 1);
+                url = url.substring(0, queryStringIndex);
+            }
 
-	private void responseBody(DataOutputStream dos, byte[] body) {
-		try {
-			dos.write(body, 0, body.length);
-			dos.flush();
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
-	}
+            if (url.startsWith("/user/create")) {
+                Map<String, String> queryMap = HttpRequestUtils.parseQueryString(queryString);
+                String name = queryMap.get("name");
+                String password = queryMap.get("password");
+                String userId = queryMap.get("userId");
+                String email = queryMap.get("email");
+
+                User user = new User(userId, password, name, email);
+                log.info("user : {}", user);
+            }
+
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
 }
