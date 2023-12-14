3c83a0317d8fe2db9fdd733e819726963c2baf03
Dev-Yesung <yeasung67@gmail.com>
2023-12-14 15:14:14 +0900

    feat: User객체에 패스워드 검증 메서드 추가 및 로그인 성공 응답 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -11,6 +11,7 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.util.Map;
 
+import db.DataBase;
 import model.User;
 
 import org.slf4j.Logger;
@@ -21,108 +22,122 @@ import util.HttpRequestUtils;
 import util.IOUtils;
 
 public class RequestHandler extends Thread {
-    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private static final String ROOT_DIRECTORY = "./webapp";
-    private final Socket connection;
+	private static final String ROOT_DIRECTORY = "./webapp";
+	private final Socket connection;
 
-    public RequestHandler(Socket connectionSocket) {
-        this.connection = connectionSocket;
-    }
+	public RequestHandler(Socket connectionSocket) {
+		this.connection = connectionSocket;
+	}
 
-    @Override
-    public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+	@Override
+	public void run() {
+		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+			connection.getPort());
 
-        try (
-                InputStream in = connection.getInputStream();
-                OutputStream out = connection.getOutputStream()
-        ) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            DataOutputStream dos = new DataOutputStream(out);
+		try (
+			InputStream in = connection.getInputStream();
+			OutputStream out = connection.getOutputStream()
+		) {
+			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			DataOutputStream dos = new DataOutputStream(out);
 
-            String line = br.readLine();
-            if (line == null) {
-                return;
-            }
-            int contentLength = 0;
-            HttpRequest httpRequest = new HttpRequest(line);
+			String line = br.readLine();
+			if (line == null) {
+				return;
+			}
+			int contentLength = 0;
+			HttpRequest httpRequest = new HttpRequest(line);
 
-            String url = httpRequest.getSimpleUrl();
-            if (httpRequest.isHtml()) {
-                byte[] body = Files.readAllBytes(new File(ROOT_DIRECTORY + url).toPath());
-                response200Header(dos, body.length);
-                responseBody(dos, body);
-            }
+			String url = httpRequest.getSimpleUrl();
+			if (httpRequest.isHtml()) {
+				responseResource(url, dos);
+			}
 
-            while (!line.equals("")) {
-                line = br.readLine();
-                if (line.startsWith("Cookie")) {
-                    Cookie cookie = new Cookie(line);
-                }
-                if (line.startsWith("Content-Length")) {
-                    contentLength = Integer.parseInt(line.split(":")[1].trim());
-                }
-            }
+			while (!line.equals("")) {
+				line = br.readLine();
+				if (line.startsWith("Cookie")) {
+					Cookie cookie = new Cookie(line);
+				}
+				if (line.startsWith("Content-Length")) {
+					contentLength = Integer.parseInt(line.split(":")[1].trim());
+				}
+			}
 
-            String requestBody = IOUtils.readData(br, contentLength);
-            if (url.startsWith("/user/create")) {
-                Map<String, String> queryMap = HttpRequestUtils.parseQueryString(requestBody);
-                String name = queryMap.get("name");
-                String password = queryMap.get("password");
-                String userId = queryMap.get("userId");
-                String email = queryMap.get("email");
+			String requestBody = IOUtils.readData(br, contentLength);
+			Map<String, String> queryMap = HttpRequestUtils.parseQueryString(requestBody);
+			if (url.startsWith("/user/create")) {
+				String name = queryMap.get("name");
+				String password = queryMap.get("password");
+				String userId = queryMap.get("userId");
+				String email = queryMap.get("email");
 
-                User user = new User(userId, password, name, email);
-                log.info("user : {}", user);
-                response302Header(dos, 0);
-            }
+				User user = new User(userId, password, name, email);
+				DataBase.addUser(user);
+				log.info("user : {}", user);
+				response302Header(dos, 0);
+			} else if (url.startsWith("/user/login")) {
+				String password = queryMap.get("password");
+				String userId = queryMap.get("userId");
+				User user = DataBase.findUserById(userId);
+				if (user.isSamePassword(password)) {
+					response200LoginSuccessHeader(dos);
+				} else {
+					responseResource("/user/login_failed.html", dos);
+				}
+			}
 
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+	private void responseResource(String url, DataOutputStream dos) throws IOException {
+		byte[] body = Files.readAllBytes(new File(ROOT_DIRECTORY + url).toPath());
+		response200Header(dos, body.length);
+		responseBody(dos, body);
+	}
 
-    private void response302Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Location: /index.html");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+		try {
+			dos.writeBytes("HTTP/1.1 200 OK \r\n");
+			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
 
-    private void response302LoginSuccessHeader(DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Location: /index.html");
-            dos.writeBytes("Set-Cookie: logined=true");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+	private void response302Header(DataOutputStream dos, int lengthOfBodyContent) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 Found \r\n");
+			dos.writeBytes("Location: /index.html");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+	private void response200LoginSuccessHeader(DataOutputStream dos) {
+		try {
+			dos.writeBytes("HTTP/1.1 200 OK \r\n");
+			dos.writeBytes("Content-Type: text/html \r\n");
+			dos.writeBytes("Set-Cookie: logined=true \r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	private void responseBody(DataOutputStream dos, byte[] body) {
+		try {
+			dos.write(body, 0, body.length);
+			dos.flush();
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
 }
